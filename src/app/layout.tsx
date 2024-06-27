cb68e4feff88964bbe55cc71849fc00c4b9c98f5
techySPHINX <shimon123universe@gmail.com>
2024-06-27 18:15:57 +0530

    Initiate Authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +16,19 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // return (
+  //   <ClerkProvider appearance={{ baseTheme: dark }}>
+  //     <main className="h-full">
+  //       {/* <Navigation /> */}
+  //       {children}
+  //     </main>
+  //   </ClerkProvider>
+  // );
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <html>
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
