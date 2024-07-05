5242471a8221b9c13afd2d22c30e7d0d2e06341f
techySPHINX <shimon123universe@gmail.com>
2024-07-05 19:38:50 +0530

    set: Frontpage layout structure

---
+++ src/app/site/page.tsx
@@ -0,0 +1,57 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+// import { pricingCards } from '@/lib/constants'
+// import { stripe } from '@/lib/stripe'
+import clsx from 'clsx'
+import { Check } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default async function Home() {
+  // const prices = await stripe.prices.list({
+  //   product: process.env.NEXT_PLURA_PRODUCT_ID,
+  //   active: true,
+  // })
+
+  return (
+    <>
+      <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col ">
+        {/* grid */}
+
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
+
+        <p className="text-center">Run your agency, in one place</p>
+        <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
+          <h1 className="text-9xl font-bold text-center md:text-[300px]">
+            Plura
+          </h1>
+        </div>
+        <div className="flex justify-center items-center relative md:mt-[-70px]">
+          <Image
+            src={'/assets/preview.png'}
+            alt="banner image"
+            height={1200}
+            width={1200}
+            className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
+          />
+          <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
+        </div>
+      </section>
+      <section className="flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px]">
+         <h2 className="text-4xl text-center"> Choose what fits you right</h2>
+        <p className="text-muted-foreground text-center">
+          Our straightforward pricing plans are tailored to meet your needs. If
+          {" you're"} not <br />
+          ready to commit you can get started for free.
+        </p>
+       
+      </section> 
+    </>
+  )
+}
